33203a390d77877c959ff3627359c74d2b5c8dfc
13zghost <13zghost@gmail.com>
2018-03-22 00:55:40 +0500

    ch2

--- Django_1st/urls.py
+++ Django_1st/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from enbase import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('enbase/', include('enbase.urls')),
+    path('', include('enbase.urls')),
 ]
